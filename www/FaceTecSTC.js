30425200826bc0b228e03b3413b4229d13b093ea
GitLab-STC <nader@shnoudi.com>
2022-08-04 18:52:11 +0300

    asdasd

--- www/FaceTecSTC.js
+++ www/FaceTecSTC.js
@@ -1,5 +1,5 @@
 /* only for parameters that we get from user */
 
 module.exports.MatchFaceID = function(successCallback, errorCallback, args) {
-    exec(successCallback, errorCallback, 'FaceTecSTC', 'MatchFaceID', args);
+    cordova.exec(successCallback, errorCallback, 'FaceTecSTC', 'MatchFaceID', args);
 }
